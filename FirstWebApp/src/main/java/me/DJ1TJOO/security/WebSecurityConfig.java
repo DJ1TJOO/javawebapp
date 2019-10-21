e23f289f6d08c98b668d8810501d7dfb433234e8
DJ1TJOO <t.ferb1@gmail.com>
2019-10-21 16:01:31 +0200

    Login

--- FirstWebApp/src/main/java/me/DJ1TJOO/security/WebSecurityConfig.java
+++ FirstWebApp/src/main/java/me/DJ1TJOO/security/WebSecurityConfig.java
@@ -1,27 +1,38 @@
 package me.DJ1TJOO.security;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
+	@Bean
+	public PasswordEncoder getPasswordEncoder() {
+		return new BCryptPasswordEncoder();
+	}
+	
 	@Override
 	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-		auth.inMemoryAuthentication().withUser("test").password("123").roles("USER");
+		auth.inMemoryAuthentication()
+		.passwordEncoder(getPasswordEncoder())
+		.withUser("test").password(getPasswordEncoder().encode("123")).roles("USER");
 	}
 	
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
-		http.csrf().disable()
+		http
 		.authorizeRequests()
 		.antMatchers("/").permitAll()
 		.anyRequest().hasRole("USER")
 		.and()
 		.formLogin()
 			.loginPage("/login")
+			.defaultSuccessUrl("/dashboard")
 			.permitAll()
 		.and()
 		.logout()
